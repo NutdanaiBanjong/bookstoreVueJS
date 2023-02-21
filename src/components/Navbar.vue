cff080e8ef50ce32c21c4589b1043dd04c132d7a
NutdanaiBanjong <natdanaibanjong@gmail.com>
2023-02-21 14:59:49 +0700

    update file

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,22 +2,21 @@
 <template>
     <nav class=" bg-[#D5B895] px-80 py-5 text-white">
         <div class="flex space-x-56 pr-80">
-            <h1 class="text-2xl">
-              <router-link 
+          <h1 class="text-2xl">
+            <router-link 
               class="focus:underline" 
               to="/">
               BookStore
-            </router-link></h1>
-        <ul class="flex gap-20 text-xl">
+            </router-link>
+          </h1>
+          <ul class="flex gap-20 text-xl">
 							<li><router-link class=" focus:underline" to="/home/list">Home</router-link> </li>
 							<li><router-link class=" focus:underline" to="/home/checkout">Checkout</router-link></li>
               <li><router-link class=" focus:underline" to="/author">Author</router-link></li>
-            </ul>
-  </div>
+          </ul>
+        </div>
     </nav>
-	<!-- <router-view/> -->
 </template>
-
 <script>
 export default {
     title: 'Navbar',
